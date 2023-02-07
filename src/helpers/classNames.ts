207decd419c49566e48568cd2ba5460f456da228
Dmytro Andoniev <dmytro.andoniev@rztech.us>
2023-02-08 00:56:01 +0200

    Setup webpack, devserver, styles and theme

---
+++ src/helpers/classNames.ts
@@ -0,0 +1,13 @@
+type ObjectClassType = Record<string, string | boolean>;
+
+export function classNames(
+    stringClasses: string,
+    objectClasses: ObjectClassType = {},
+    additionalClasses: string[] = []
+): string {
+    return [
+        stringClasses,
+        ...Object.keys(objectClasses).filter((key) => !!objectClasses[key]),
+        ...additionalClasses
+    ].join(" ");
+}
