bd1e7a77555dd7163cabc1001cf89050f45acf4c
Dmytro Andoniev <dmytro.andoniev@rztech.us>
2023-04-30 12:47:11 +0300

    Fixed api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 import { USER_LOCAL_STORAGE_KEY } from "shared/const/localStorage";
 
 export const $api = axios.create({
-    baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) ?? ""
+    baseURL: __API__
+});
+
+$api.interceptors.request.use((config) => {
+    const newConfig = { ...config };
+    if (newConfig.headers) {
+        newConfig.headers.authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) ?? "";
     }
+
+    return newConfig;
 });
